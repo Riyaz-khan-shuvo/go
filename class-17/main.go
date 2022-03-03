da681aaec2b5f28fae49d1f12865409d3ad80eb5
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-03 16:12:10 +0600

    class-17 complete

---
+++ class-17/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello I am working!!!")
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":8888", nil)
+
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello I am from go lang and this is my first run code in the web browser")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is about page")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello I am from contact page ")
+}
