b8451edfd0b127d3bccfe50ef0964ec06fb431da
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-12 18:09:11 +0600

    practice complete

--- practice-18/main.go
+++ practice-18/main.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello I am working")
+	fmt.Println("Hello I am working!!!")
 
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/features", featurePage)
+	http.HandleFunc("/docs", docsPage)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./loruki"))))
+
 	http.ListenAndServe(":8888", nil)
+
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am from practice page ")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello I am from home page ")
+	pToTem, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pToTem.Execute(w, nil)
 }
+
+func featurePage(w http.ResponseWriter, r *http.Request) {
+	pToTem, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pToTem, err = pToTem.ParseFiles("./wpages/feature.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pToTem.Execute(w, nil)
+}
+
+func docsPage(w http.ResponseWriter, r *http.Request) {
+	pToTemp, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pToTemp, err = pToTemp.ParseFiles("./wpages/docs.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pToTemp.Execute(w, nil)
+}
+
+// func about(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "I am from about page ")
+// }
